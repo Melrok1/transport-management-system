11b5934a74ca615bb04e86f036883ea5ff560845
Martin Blaščák <martin.blascak86@gmail.com>
2025-07-09 18:26:41 +0200

    backend - added filters by name and date

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Order, Waypoint
 from .serializers import OrderSerializer, WaypointSerializer
 
@@ -11,3 +11,9 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class WaypointCreateView(generics.CreateAPIView):
     queryset = Waypoint.objects.all()
     serializer_class = WaypointSerializer
+    
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['customer_name', 'date']
